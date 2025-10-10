ea1a374305cba887d0c2ba5a309f9ea364f09929
MagetoJ <jmageto@statbricks.com>
2025-10-10 23:21:35 +0300

    status

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/react-app/index.html",
-    "./src/react-app/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
     extend: {
       screens: {
